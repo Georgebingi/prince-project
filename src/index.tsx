7c3b96b4dbd39a8d6f1d7eb0413ba4492ca45fb0
Your Name <you@example.com>
2026-02-03 23:47:30 +0100

    fixed merge conflicts and cleaned files

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import './index.css';
-<<<<<<< HEAD
+
 import React from "react";
 import { render } from "react-dom";
 import { App } from "./App";
 
 render(<App />, document.getElementById("root"));
-=======
+
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
+
 
 const container = document.getElementById("root");
 if (!container) {
@@ -16,4 +16,4 @@ if (!container) {
 
 const root = createRoot(container);
 root.render(<App />);
->>>>>>> 57aaee95c582e73f35a15cb51cf06fbe324c181e
+
